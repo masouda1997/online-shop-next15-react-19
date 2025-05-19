d62a4ee8b173ad0116656f3531d554b689c3a77a
Masoud anaraki <masoud30376@gmail.com>
2025-05-19 09:39:31 +0330

    test: test with github desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Welcome from '@/components/Welcome';
 import React from 'react';
 
 export default function Home() {
-	console.log('this it a log');
 	return (
 		<div className="flex flex-col items-center">
 			<Banner />
