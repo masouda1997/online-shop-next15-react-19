acf1450ac4a14257d4983c6bb30a05ff2d0aeae0
Masoud anaraki <masoud30376@gmail.com>
2025-05-19 11:10:34 +0330

    wroong format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Banner } from '@/components/banner';
 import Welcome from '@/components/Welcome';
 
 import React from 'react';
-
+console.log('object');
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center">
